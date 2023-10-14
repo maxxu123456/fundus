4cdedfa3dbe139b02458f062d5265217407028b3
Max Xu <maxxu123456@gmail.com>
2023-10-13 23:16:09 -0400

    added Post.js and added test code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Post from "./components/post/Post";
 
 function App() {
+  let post1 = {
+    postTitle: "Need paper towel",
+    postDescription:
+      "We need to put paper towels inside the restroom because there's nothing",
+    cost: 5.0,
+    minPeople: 2,
+    //hours
+    maxPostTime: 12,
+    maxDeliveryTime: 13,
+  };
+  let post2 = {
+    postTitle: "Microwave",
+    postDescription: "We need microwave inside the dorm floor kitchen",
+    cost: 150.0,
+    minPeople: 10,
+    maxPostTime: 23,
+    maxDeliveryTime: 22,
+  };
+  let posts = [];
+  posts.push(post1);
+  posts.push(post2);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {posts.map(function (post) {
+        return <Post post={post}></Post>;
+      })}
     </div>
   );
 }
---
+++ src/components/post/Post.js
@@ -0,0 +1,17 @@
+function Post(props) {
+  return (
+    <div>
+      <p>{"Title: " + props.post.postTitle}</p>
+      <p>{"Description: " + props.post.postDescription}</p>
+      <p>{"Cost: " + props.post.cost}</p>
+      <p>{"Minimum Number of People Required: " + props.post.minPeople}</p>
+      <p>{"Maximum Amount for people to Pledge " + props.post.maxPostTime}</p>
+      <p>
+        {"Max Amount of Poster to deliver after enough people pledge " +
+          props.post.maxDeliveryTime}
+      </p>
+    </div>
+  );
+}
+
+export default Post;
