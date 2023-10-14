7f040b0caba956a9dcd63045efebf638775edf29
Max Xu <maxxu123456@gmail.com>
2023-10-14 00:04:15 -0400

    added basic form fields for signup

--- src/components/signup/Signup.js
+++ src/components/signup/Signup.js
@@ -1,7 +1,30 @@
+import "./Signup.css";
+
 function Signup(props) {
   return (
     <div>
-      <p>Signup Page</p>
+      <div>
+        <label>First Name: </label>
+        <input></input>
+      </div>
+      <div>
+        <label>Last Name: </label>
+        <input></input>
+      </div>
+      <div>
+        <label>Location: </label>
+        <input></input>
+      </div>
+      <div>
+        <label>Email: </label>
+        <input></input>
+      </div>
+      <div>
+        <label>Create Password: </label>
+        <input></input>
+      </div>
+
+      <button>Create Account</button>
     </div>
   );
 }
