c2b89047c547e3b71d43521c6e2e4ab05f2d7009
Max Xu <maxxu123456@gmail.com>
2023-10-13 23:17:59 -0400

    added firebase to project

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAgAcnWhLzLyjFIpv9dtd9gMMIaNumjmpw",
+  authDomain: "fundus-bbba5.firebaseapp.com",
+  projectId: "fundus-bbba5",
+  storageBucket: "fundus-bbba5.appspot.com",
+  messagingSenderId: "101016243752",
+  appId: "1:101016243752:web:14abc3c011ccc334c16f1d",
+  measurementId: "G-GE7L022MM0",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
